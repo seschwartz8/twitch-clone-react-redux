56a1eef4d2269e2786585cdcf8831e69db4c3117
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-26 10:28:21 -0700

    create buttons with handlers to sign in/out on click

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -25,13 +25,31 @@ class GoogleAuth extends Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we're signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I'm signed in</div>;
+      return (
+        <button className='ui red google button' onClick={this.onSignOut}>
+          <i className='google icon' />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div> Not signed in</div>;
+      return (
+        <button className='ui blue google button' onClick={this.onSignIn}>
+          <i className='google icon' />
+          Sign In with Google
+        </button>
+      );
     }
   }
 
