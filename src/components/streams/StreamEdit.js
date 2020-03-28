5021c646cfa89630484cb461e087e12fea072f8f
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-28 13:11:17 -0700

    fetch stream within StreamEdit to keep it from relying on another route fetching data

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = props => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends Component {
+  componentDidMount() {
+    // Use the props that router automatically passes to rendered components (it contains the params from the routes, such as :id)
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    return <div>StreamEdit</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const specificId = ownProps.match.params.id;
+  // Add the specific stream to component props
+  return { stream: state.streams[specificId] };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
